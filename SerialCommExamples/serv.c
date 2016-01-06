ff4c891b42e17d41b24be953ef18fc0e166a7cd5
LeeKeunCheol <dlrmscjf5857@gmail.com>
2016-01-06 11:15:12 +0900

    Add Serial comm example

---
+++ SerialCommExamples/serv.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <fcntl.h>
+
+int main( int argc, char** argv)
+{
+	int fd;				// file descripter ( serial descripter )
+	char buf[20] = "";		// message buffer
+	char name[20] = "/dev/pts/";	// virtual port name
+
+	struct termios newtio;          // value for setting terminal 
+
+	if( argc != 2 )
+	{
+		printf("Usage : %s <portNum>\n", argv[0]);
+		exit(1);
+	}		
+	
+	strcat( name, argv[1] );	// set port name string
+	
+//	fd = open( name, O_RDWR | O_NOCTTY | O_NONBLOCK );
+	fd = open( name, O_RDWR | O_NOCTTY );
+	// O_RDWR	: read and write mode
+	// O_NOCTTY	: ?
+	// O_NONBLOCK	: if no read data, no waiting
+
+        if ( fd == -1 )
+        {
+                printf("Port number is incorrect.\n");
+                exit(1);
+        }
+
+	memset( &newtio, 0, sizeof(newtio) );
+
+	newtio.c_cflag = B115200;   // 115200 comm speed
+	newtio.c_cflag |= CS8;      // 8bit data bit
+	newtio.c_cflag |= CLOCAL;   // use inner comm port
+	newtio.c_cflag |= CREAD;    // read and write
+	newtio.c_iflag = 0;         // none parity
+	newtio.c_oflag = 0;
+	newtio.c_lflag = 0;
+	newtio.c_cc[VTIME] = 0;
+	newtio.c_cc[VMIN] = 1;
+
+	tcflush (fd, TCIFLUSH );
+	tcsetattr(fd, TCSANOW, &newtio );
+	// comm option setting
+
+	while (1)
+	{
+		read ( fd, buf,  20);
+		// read data in 'fd' port
+		printf("buf is [%s]\n", buf);
+	}
+	close( fd);
+	// close port
+}
